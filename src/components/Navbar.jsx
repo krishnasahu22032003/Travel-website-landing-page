62e01d2a0954d42c489e578eea7ab35c185575ed
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-29 00:52:02 +0530

    Tailwind classes bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
+import { NAV_LINKS } from "../../constant"
 
 
 const Navbar = () => {
   return (
-<nav className=" flex-between max-container padding-container relative z-30 py-5">
+<nav className=" flexBetween max-container padding-container relative z-30 py-10 px-10">
 <a href="/">
-<img src="/images/logo.png" alt="logo" className="w-[70px] h-[70px] pt-0 " />
-<ul className="hidden h-full gap-12 lg:flex" >
-
-</ul>
+<img src="/images/logo.png" alt="logo" className="w-[100px] h-[70px] top-0 px-5 " />
 </a>
+<ul className="hidden h-full gap-12 lg:flex" >
+{NAV_LINKS.map((link)=>(
+<a href={link.href} key={link.key} className="cursor-pointer flexCenter regular-16 text-gray-400 pb-1.5 transition-all hover:font-bold "  >
+{link.label}
+
+</a>
+
+))}
+</ul>
+
 </nav>
   )
 }
