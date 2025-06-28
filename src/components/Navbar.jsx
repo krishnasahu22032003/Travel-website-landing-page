128721960b4f5631776d579ce293ec4a3c1cf91a
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-29 01:16:30 +0530

    Navbar bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,46 +3,49 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center max-w-[1440px] mx-auto px-10 py-5 relative z-30">
+    <nav className="flex items-center justify-between max-w-[1440px] mx-auto px-6 lg:px-12 py-5">
       {/* Logo */}
-      <a href="/">
+      <a href="/" className="shrink-0">
         <img
           src="/images/logo.png"
           alt="logo"
-          className="w-[100px] h-[70px] px-5 object-contain"
+          className="w-[100px] h-[60px] object-contain"
         />
       </a>
 
-      {/* Navigation Links */}
-      <ul className="hidden lg:flex h-full gap-12 items-center">
-        {NAV_LINKS.map((link) => (
-          <li key={link.key}>
-            <a
-              href={link.href}
-              className="text-gray-400 text-[16px] font-normal pb-1.5 hover:font-bold transition-all"
-            >
-              {link.label}
-            </a>
-          </li>
-        ))}
-      </ul>
+      {/* Nav + Button Group */}
+      <div className="hidden lg:flex items-center gap-8">
+        {/* Navigation Links */}
+        <ul className="flex items-center gap-8">
+          {NAV_LINKS.map((link) => (
+            <li key={link.key}>
+              <a
+                href={link.href}
+                className="text-gray-400 text-[16px] font-normal hover:font-bold transition-all"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
 
-      {/* Login Button */}
-      <div className="hidden lg:flex items-center justify-center">
+        {/* Login Button */}
         <Button
           type="button"
           title="Login"
-          icon="/user.svg"
+          icon="/images/user.svg"
           variant="btn_dark_green"
         />
       </div>
 
       {/* Mobile Menu Icon */}
-      <img
-        src="/images/menu.svg"
-        alt="menu"
-        className="block lg:hidden w-8 h-8 cursor-pointer"
-      />
+      <div className="lg:hidden">
+        <img
+          src="/images/menu.svg"
+          alt="menu"
+          className="w-8 h-8 cursor-pointer"
+        />
+      </div>
     </nav>
   );
 };
