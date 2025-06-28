f5562bd3dc177a22a8a800042e843ddfbdac7bbd
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-29 00:16:26 +0530

    Navlink added in constant index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Navbar from './components/Navbar'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     <div className='bg-green-90 text-black'>
-      hwllo hi there
-     </div>
+   <Navbar/>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar = () => {
+  return (
+<nav className=" flex-between max-container padding-container relative z-30 py-5">
+<a href="/">
+<img src="/images/logo.png" alt="logo" className="w-[70px] h-[70px] pt-0 " />
+<ul className="hidden h-full gap-12 lg:flex" >
+
+</ul>
+</a>
+</nav>
+  )
+}
+
+export default Navbar
