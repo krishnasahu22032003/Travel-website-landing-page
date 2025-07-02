f3fe25eadf253fe0293c8bd564e0d9b2ecf0d05b
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-02 23:30:48 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,48 +3,51 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="relative z-50 flex items-center  justify-between max-w-[1440px] mx-auto px-6 lg:px-12 py-5">
-      {/* Logo */}
-      <a href="/" className="shrink-0">
-        <img
-          src="/images/logo.png"
-          alt="logo"
-          className="w-[100px] h-[60px] object-contain"
-        />
-      </a>
+    <nav className="relative z-50 w-full bg-transparent">
+      <div className="flex items-center justify-between max-w-[1440px] mx-auto px-6 lg:px-12 py-5">
+        
+        {/* Logo */}
+        <a href="/" className="shrink-0" aria-label="Go to homepage">
+          <img
+            src="/images/logo.png"
+            alt="Globeora Logo"
+            className="w-[120px] h-[70px] object-contain"
+          />
+        </a>
 
-      {/* Nav + Button Group */}
-      <div className="hidden lg:flex items-center gap-8">
-        {/* Navigation Links */}
-        <ul className="flex items-center gap-8">
-          {NAV_LINKS.map((link) => (
-            <li key={link.key}>
-              <a
-                href={link.href}
-                className="text-black  text-[16px] font-normal hover:font-bold transition-all"
-              >
-                {link.label}
-              </a>
-            </li>
-          ))}
-        </ul>
+        {/* Desktop Navigation */}
+        <div className="hidden lg:flex items-center gap-10">
+          <ul className="flex items-center gap-8">
+            {NAV_LINKS.map((link) => (
+              <li key={link.key}>
+                <a
+                  href={link.href}
+                  className="text-black text-[16px] font-medium hover:text-green-70 transition-colors duration-200"
+                >
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
 
-        {/* Login Button */}
-        <Button
-          type="button"
-          title="Login"
-          icon="/images/user.svg"
-          variant="btn_dark_green"
-        />
-      </div>
+          <Button
+            type="button"
+            title="Login"
+            icon="/images/user.svg"
+            variant="btn_dark_green"
+          />
+        </div>
 
-      {/* Mobile Menu Icon */}
-      <div className="lg:hidden">
-        <img
-          src="/images/menu.svg"
-          alt="menu"
-          className="w-8 h-8 cursor-pointer"
-        />
+        {/* Mobile Menu Icon */}
+        <div className="lg:hidden">
+          <button aria-label="Open menu">
+            <img
+              src="/images/menu.svg"
+              alt="Menu"
+              className="w-8 h-8 cursor-pointer"
+            />
+          </button>
+        </div>
       </div>
     </nav>
   );
